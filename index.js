871dd086fe3a4b970d917f85ee2530b75ddb48a4
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-04-03 13:46:43 +0100

    3.3 done

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ const persons = [
 ];
 
 
+app.get('/api/persons/:id', (req, res) => { 
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
